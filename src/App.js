a3405889b9e5ce0fd14779f53cfb787bcca2580e
parkhank <park.hty@gmail.com>
2020-06-05 10:47:00 -0400

    finish track given position prop from parent, minimal styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.scss';
+import Tracks from './components/Tracks';
+
+const fillerPositionData = { turnips: 3, celery: 3, onions: 3 };
 
 class App extends React.Component {
 
 render() {
   return (
-    <h1 className="app">
-      Filler
-    </h1>
+    <div className="app">
+      <h1 className="app__header">Stalk Market</h1>
+      <Tracks positions={fillerPositionData}/>
+    </div>
   );
 }
 
